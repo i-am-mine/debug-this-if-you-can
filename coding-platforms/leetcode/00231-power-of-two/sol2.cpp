cb2b3b84a8db9c210cd17960697bf5ce8514f80c
Minh Nguyen <nghoangminh1505@gmail.com>
2025-08-17 22:44:12 +0200

    [leetcode] update solutions for Power of two

--- coding-platforms/leetcode/00231-power-of-two/sol2.cpp
+++ coding-platforms/leetcode/00231-power-of-two/sol2.cpp
@@ -4,5 +4,5 @@ using namespace std;
 // Constraints: -2^31 <= n <= 2^31 - 1
 
 bool isPowerOfTwo(int n) {
-    return n <= 0 ? false : (n & (n-1)) == 0; // n is power of 2 iff (n & (n-1)) == 0
+    return n > 0 && (n & (n-1)) == 0; // n is power of 2 iff (n & (n-1)) == 0
 }
--- coding-platforms/leetcode/00231-power-of-two/sol3.cpp
+++ coding-platforms/leetcode/00231-power-of-two/sol3.cpp
@@ -4,5 +4,5 @@ using namespace std;
 // Constraints: -2^31 <= n <= 2^31 - 1
 
 bool isPowerOfTwo(int n){
-    return n <= 0 ? false : 1073741824 % n == 0; // 2^30 = 1073741824
+    return n > 0 && 1073741824 % n == 0; // 2^30 = 1073741824
 }
