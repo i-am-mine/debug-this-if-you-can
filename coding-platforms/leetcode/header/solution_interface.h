19c96046bf823111a51a26501fd7d319c432c4d8
Minh Nguyen <nghoangminh1505@gmail.com>
2025-08-20 01:51:06 +0200

    [leetcode] Update header

--- coding-platforms/leetcode/header/solution_interface.h
+++ coding-platforms/leetcode/header/solution_interface.h
@@ -8,5 +8,6 @@ std::vector<int> twoSum(std::vector<int>& nums, int target);
 bool isPowerOfTwo(int n);
 bool isPowerOfThree(int n);
 bool isPowerOfFour(int n);
+long long zeroFilledSubarray(std::vector<int>& nums)
 
 
