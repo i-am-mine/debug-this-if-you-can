899ae6ca9ae9224921ad98f68452b30ec4a73428
Minh Nguyen <nghoangminh1505@gmail.com>
2025-08-20 23:21:46 +0200

    [leetcode] update interface

--- coding-platforms/leetcode/header/solution_interface.h
+++ coding-platforms/leetcode/header/solution_interface.h
@@ -8,6 +8,7 @@ std::vector<int> twoSum(std::vector<int>& nums, int target);
 bool isPowerOfTwo(int n);
 bool isPowerOfThree(int n);
 bool isPowerOfFour(int n);
+int countSquares(std::vector<std::vector<int>>& matrix)
 long long zeroFilledSubarray(std::vector<int>& nums)
 
 
