9c010abf482a75803bf032cacbf560d7934d4266
Minh Nguyen <nghoangminh1505@gmail.com>
2025-09-14 17:25:54 +0200

    [leetcode] update interface

--- coding-platforms/leetcode/header/solution_interface.h
+++ coding-platforms/leetcode/header/solution_interface.h
@@ -3,12 +3,21 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-std::vector<int> twoSum(std::vector<int>& nums, int target);
 bool isPowerOfTwo(int n);
 bool isPowerOfThree(int n);
 bool isPowerOfFour(int n);
-int countSquares(std::vector<std::vector<int>>& matrix)
-long long zeroFilledSubarray(std::vector<int>& nums)
 
+int countSquares(std::vector<std::vector<int>>& matrix);
+int maxFreqSum(string s);
+int findClosest(int x, int y, int z);
 
+long long zeroFilledSubarray(std::vector<int>& nums);
+
+void rotate(std::vector<int>& nums, int k);
+
+std::vector<int> twoSum(std::vector<int>& nums, int target);
