812c67cd1964260c93a175d04585b29942960d59
Minh Nguyen <nghoangminh1505@gmail.com>
2025-10-13 23:02:51 +0200

    [leetcode] update interface

--- coding-platforms/leetcode/header/solution_interface.h
+++ coding-platforms/leetcode/header/solution_interface.h
@@ -1,18 +1,29 @@
 #include <bits/stdc++.h>
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 bool isPowerOfTwo(int n);
 bool isPowerOfThree(int n);
 bool isPowerOfFour(int n);
+bool isPalindrome(int x);
 
-int canBeTypedWords(string text, string brokenLetters)
+int canBeTypedWords(string text, string brokenLetters);
 int countSquares(std::vector<std::vector<int>>& matrix);
 int findClosest(int x, int y, int z);
 int maxFreqSum(string s);
-int countStudents(vector<int>& students, vector<int>& sandwiches)
+int countStudents(vector<int>& students, std::vector<int>& sandwiches);
+int lengthOfLongestSubstring(string s);
 
 long long zeroFilledSubarray(std::vector<int>& nums);
 
 void rotate(std::vector<int>& nums, int k);
 
 std::vector<int> twoSum(std::vector<int>& nums, int target);
-std::vector<int> getNoZeroIntegers(int n)
+std::vector<int> getNoZeroIntegers(int n);
+
