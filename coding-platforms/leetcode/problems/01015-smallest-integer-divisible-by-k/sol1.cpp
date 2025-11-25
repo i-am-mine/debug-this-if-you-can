7b50b0b47d7d0fb7629a1208baf830561c2ba7cf
Minh Nguyen <nghoangminh1505@gmail.com>
2025-11-25 20:33:28 +0100

    [leetcode] add solutions of the problem 1015

---
+++ coding-platforms/leetcode/problems/01015-smallest-integer-divisible-by-k/sol1.cpp
@@ -0,0 +1,15 @@
+#include "../header/solution_interface.h"
+using namespace std;
+
+int smallestRepunitDivByK(int k) {
+    if (k == 1) return 1;
+    if (k == 11) return 2;
+    if (k % 2 == 0 || k % 5 == 0) return -1;
+    int r = 0;
+    for (int len = 1; len <= k; ++len) {
+        r = (r * 10 + 1) % k;
+        if (r == 0) return len;
+    }
+
+    return -1;
+}
